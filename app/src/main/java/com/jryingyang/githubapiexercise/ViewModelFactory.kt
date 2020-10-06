b794cf168028091f2fed7ef18d90acfe6293ebb5
ChihYing Yang <jrying1992@gmail.com>
2020-10-06 19:01:30 +0800

    [Feature] Add First Page: User List

---
+++ app/src/main/java/com/jryingyang/githubapiexercise/ViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.jryingyang.githubapiexercise
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.jryingyang.accessappexercise.data.GithubRepository
+
+class ViewModelFactory(private val repository: GithubRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
